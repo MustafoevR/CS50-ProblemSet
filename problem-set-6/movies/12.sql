8a5d0aa085e6491a81f01390c64c7c52676f6b88
Rustam <77645108+MustafoevR@users.noreply.github.com>
2021-03-28 23:48:30 +0500

    SQL

---
+++ problem-set-6/movies/12.sql
@@ -0,0 +1,1 @@
+SELECT title FROM movies WHERE id IN (SELECT stars.movie_id FROM people JOIN stars ON people.id = stars.person_id WHERE name = "Johnny Depp") INTERSECT SELECT title FROM movies WHERE id IN (SELECT stars.movie_id FROM people JOIN stars ON people.id = stars.person_id WHERE name = "Helena Bonham Carter");
